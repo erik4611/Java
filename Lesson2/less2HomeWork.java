f652bdd6d551df947f61c7458aada28d15d30e2b
erik <erik4611@yandex.ru>
2020-09-30 21:34:58 +0300

    Exercises 1-7 lesson 2

--- Lesson2/less2HomeWork.java
+++ Lesson2/less2HomeWork.java
@@ -41,8 +41,28 @@ public class less2HomeWork {
         // минимальный и максимальный элементы (без помощи интернета);
 
         int[] arr5 = new int[] {1, 5, 3, 2, 11, 4};
+        minAndMaxInArray(arr5);
 
+        // Задание 6: Написать метод, в который передается не пустой
+        // одномерный целочисленный массив, метод должен вернуть true,
+        // если в массиве есть место, в котором сумма левой и правой
+        // части массива равны. Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
+        // checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||,
+        // эти символы в массив не входят.
 
+        int[] arr6 = new int[] {4, 5, 1, 2, 6, 6};
+        int[] arr7 = new int[] {1, 12, 1, 2, 3, 3};
+        checkBalance(arr6);
+        checkBalance(arr7);
+
+        // Задание 7: Написать метод, которому на вход подается одномерный массив
+        // и число n (может быть положительным, или отрицательным), при этом метод
+        // должен сместить все элементымассива на n позиций. Для усложнения задачи
+        // нельзя пользоваться вспомогательными массивами.
+
+        int[] arr8 = new int[] {1, 2, 3, 4, 5, 9, 122};
+        int n = 4;
+        rotateToN(arr8, n);
 
     }
 
@@ -84,10 +104,56 @@ public class less2HomeWork {
         }
     }   //метод для задания 4.
 
-    static void minAndMaxInArray (int[] arr) {
-        int a = Math.max(Math.max (arr[0], Math.max(arr[1], Math.max(arr[2], Math.max(arr[3], Math.max(arr[4], arr[5]))))));
-        System.out.println(a);
-        int b = Math
-    }
+    static void minAndMaxInArray (int[] arr) {  //метод для задания 5.
+        int a = Math.max(arr[0], Math.max(arr[1], Math.max(arr[2], Math.max(arr[3], Math.max(arr[4], arr[5])))));
+        System.out.println("Задание 5.1: Максимальное значение массива - " + a);
+        int b = Math.min(arr[0], Math.min(arr[1], Math.min(arr[2], Math.min(arr[3], Math.min(arr[4], arr[5])))));
+        System.out.println("Задание 5.2: Минимальное значение массива - " + b);
+    }   //метод для задания 5.
+
+    static void checkBalance (int[] arr) {  //метод для задания 6.
+        int x = 0;
+        int z = 0;
+        for (int i = 0; i < arr.length; i++) {
+            x += arr[i];                                             //сумма всех членов массива
+            z = x - (arr[arr.length - 1] + arr[arr.length - 2]);        //сумма членов массива без двух последних
+        } if (x - z == z) {
+            System.out.println("Задание 6: true");
+        } else {
+            System.out.println("Задание 6: false");
+        }
+    }       //метод для задания 6.
+
+    static void rotateToN (int[] arr, int n) {         ////метод для задания 7.
+        if (n != 0) {
+            int s;                                   // Избавляемся от возможности n превысить длину массива
+            if (n > arr.length) {
+                n = Math.abs(n % arr.length);
+            } else {
+                s = n;
+            } if (n > 0) {                             // Сдвиг слева направо при n > 0
+                for (int k = 0; k < n; k++) {
+                    int buffer1 = arr[0];                //записываем в буфер первый элемент
+                    arr[0] = arr[arr.length - 1];       //на место первого ставим последний элемент
+                    for (int j = 1; j < arr.length - 1; j++) {              //сдвиг массива циклом
+                        arr[arr.length - 1] = arr[arr.length -j - 1];
+                    }   arr[1] = buffer1;
+
+                }
+            }
+        }   else if (n < 0) {
+            for (int i = 0; i > n; n--) {
+                int buffer2 = arr[arr.length - 1];
+                arr[arr.length - 1] = arr[0];
+
+                for (int j = 1; j < arr.length - 1; j++) {
+                    arr[j - 1] = arr[j];
+                    arr[arr.length - 2] = buffer2;
+                }
+            }
+        }
+        System.out.println("Задание 7: " + Arrays.toString(arr));
+    }   ////метод для задания 7.
 
 }
+
